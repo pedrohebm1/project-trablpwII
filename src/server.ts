e4f6dd32cfd72b02839873b35bbf6582a4683e3e
Pedro Henrique <101569708+pedrohebm1@users.noreply.github.com>
2023-09-04 23:51:29 -0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,26 @@ const app = fastify();
 
 const prisma = new PrismaClient();
 
-app.get('/users', async () => {
-    const users = await prisma.user.findMany();
-    return users
+app.listen({
+    port:3333,
+}).then(() => {
+    console.log('HTTP server running on http://localhost:3333')
 })
 
-app.listen({port: 3333}).then(() => {
-    console.log('HTTP server running on http://localhost:3333')
+app.get('/students', async () => {
+    const students = await prisma.students.findMany()
+
+    return students
+})
+
+app.get('/teachers', async () => {
+    const teachers = await prisma.teachers.findMany()
+
+    return teachers
+})
+
+app.get('/subjects', async () => {
+    const subjects = await prisma.subjects.findMany()
+
+    return subjects
 })
